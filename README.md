fb60c30b338f6bddbd559e2dce48f9f34c19778f
km singh <kmsingh0999@gmail.com>
2024-11-21 21:54:28 +0530

    "first commit"

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # US-VISA
 
 MLOPS_Production-End to End Project
+
+## How to run
+
+1. conda create -n visa python=3.12 -y
+
+2.conda activate visa
+
+3.pip install -r "requirements.txt"
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup,find_packages
+setup(
+  name="us-visa",
+  version="0.0.0",
+  author="kmsingh",
+  author_email="kmsingh0999@gmail.com",
+  packages=find_packages()
+)
